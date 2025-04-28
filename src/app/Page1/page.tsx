031191088d1ce40fc46eaabaec2a47d2634b1866
a00840350 <a00840350@tec.mx>
2025-04-28 10:22:03 -0600

    Modified repo

--- src/app/Page1/page.tsx
+++ src/app/Page1/page.tsx
@@ -17,7 +17,7 @@ export default function Page1() {
       <main className="p-8 bg-white rounded-2xl shadow-md w-full max-w-md text-center">
         {nombre ? (
           <>
-            <h1 className="text-3xl font-bold mb-4">¡Hola, {nombre}!</h1>
+            <h1 className="text-3xl font-bold mb-4">Bienvenido, {nombre}</h1>
             <p className="text-lg">disfruta tu experiencia en GreenPark.</p>
           </>
         ) : (
